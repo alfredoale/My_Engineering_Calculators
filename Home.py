b8f76bca3aceebda3db7ac5973685bd03a3912b6
alfredoale <alfredoaguirre@outlook.com>
2024-06-09 23:13:00 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,8 +10,8 @@ st.markdown("""
         <footer style="position: fixed; left: 0; bottom: 0; width: 100%; 
             background-color: #f8f9fa; color: #212529; text-align: center;
             padding: 10px 0; border-top: 1px solid #e9ecef;">
-            By using this site, you agree to the <a href="" style="text-decoration: none; color: #007bff;">Terms and Conditions</a>. 
-            This site is licensed under the <a href="" style="text-decoration: none; color: #007bff;">MIT License</a>. 
-            Please review the <a href="" style="text-decoration: none; color: #007bff;">Privacy Policy</a>.
+            By using this site, you agree to the <a href="https://raw.githubusercontent.com/alfredoale/My_Engineering_Calculators/main/terms_of_use.md" style="text-decoration: none; color: #007bff;">Terms and Conditions</a>. 
+            This site is licensed under the <a href="https://raw.githubusercontent.com/alfredoale/My_Engineering_Calculators/main/LICENSE.txt" style="text-decoration: none; color: #007bff;">MIT License</a>. 
+            Please review the <a href="https://raw.githubusercontent.com/alfredoale/My_Engineering_Calculators/main/privacy_policy.md" style="text-decoration: none; color: #007bff;">Privacy Policy</a>.
         </footer>
     """, unsafe_allow_html=True)
