f24f1540a761863d5452e4940d4499008d0a0278
alfredoale <alfredoaguirre@outlook.com>
2026-09-12 07:50:00 -0400

    Update __init__.py

--- calculator_scripts/__init__.py
+++ calculator_scripts/__init__.py
@@ -3,7 +3,7 @@ from .ontario_building_code_2024.version_2025_01.part_9.specified_snow_load impo
 from .ontario_building_code_2024.version_2025_01.part_9.maximum_floor_joist_span import maximum_floor_joist_span_calculator
 from .data_table import data_table_calculators
 from .structural_analysis.beam_reactions import beam_calculator
-from .ontario_building_code_2024.version_2025_01.part_9.sb1_climatic_and_seismic_data.wind_velocity_pressure_calculator import wind_velocity_pressure_calculator
+from .ontario_building_code_2024.version_2025_01.sb1_climatic_and_seismic_data.wind_velocity_pressure_calculator import wind_velocity_pressure_calculator
 
 CALCULATORS = {
     snow_load_calculator.title: snow_load_calculator,
